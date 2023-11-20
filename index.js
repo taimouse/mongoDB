26a32de2d17eae74cc978e8092b71b6de2904426
taimouse <40199468+taimouse@users.noreply.github.com>
2023-11-20 17:31:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const port = 5000
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const { Item } = require('./models/Item')
+// const { Item } = require('./models/Item')
 const cors = require("cors");
 const config = require('./config/key')
 
@@ -16,25 +17,5 @@ mongoose.connect(config.mongoURI)
 
 app.get('/', (req, res) => res.send('Cooltrack'))
 
-app.get('/items', (req, res) => {
-	Item.find()
-			.then(items => {
-					res.status(200).json(items);
-			})
-			.catch(err => {
-					res.status(500).send('Error occurred while fetching items', err);
-			});
-});
 
-app.post('/list', (req, res) => {
-	const item = new Item(req.body)
-	item.save()
-	.then(savedItem => {
-		console.log('Item saved successfully: ', savedItem);
-		res.status(200).send('Item saved successfully!');
-	})
-	.catch(err => {
-			console.log('Error while saving the item: ', err);
-			res.status(500).send('Item could not be saved');
-	});
-})
+app.listen(port, () => console.log(`Listening on port ${port}`))
